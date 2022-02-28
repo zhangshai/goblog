a497650ff64c17171e2083f0d052b339ca718600
zhangshaohai <>
2022-02-28 11:54:12 +0800

    添加404幢吗

--- main.go
+++ main.go
@@ -6,11 +6,14 @@ import (
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	if r.URL.Path == "/" {
 		fmt.Fprint(w, "<h1>Hello, 这里是 goblog</h1>")
 	} else if r.URL.Path == "/about" {
-		fmt.Fprint(w, "<h1>此博客是用以记录编程笔记，如您有反馈或建议</h1><a href='https://www.baidu.com'>baidu</a>")
+		fmt.Fprint(w, "此博客是用以记录编程笔记，如您有反馈或建议，请联系 "+
+			"<a href=\"mailto:summer@example.com\">summer@example.com</a>")
 	} else {
+		w.WriteHeader(http.StatusNotFound)
 		fmt.Fprint(w, "<h1>无法找到页面</h1>")
 	}
 
