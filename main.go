ed93a6c12545939b59e9a9800d737c1580ab45a7
zhangshaohai <>
2022-02-28 10:52:40 +0800

    新增页面

--- main.go
+++ main.go
@@ -6,7 +6,14 @@ import (
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1> Hello ,这里是 goblog</h1>")
+	if r.URL.Path == "/" {
+		fmt.Fprint(w, "<h1>Hello, 这里是 goblog</h1>")
+	} else if r.URL.Path == "/about" {
+		fmt.Fprint(w, "<h1>此博客是用以记录编程笔记，如您有反馈或建议</h1><a href='https://www.baidu.com'>baidu</a>")
+	} else {
+		fmt.Fprint(w, "<h1>无法找到页面</h1>")
+	}
+
 }
 
 func main() {
