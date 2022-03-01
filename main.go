4fbcc11631cd31c5ca21fc4dbbd42b3a129e0c39
zhangshaohai <>
2022-03-01 15:23:19 +0800

    使用模板文件

--- main.go
+++ main.go
@@ -14,6 +14,12 @@ import (
 //定义路由
 var route = mux.NewRouter()
 
+type ArticlesFormData struct {
+	Title, Body string
+	URL         *url.URL
+	Errors      map[string]string
+}
+
 func notFoundHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusNotFound)
 	fmt.Fprint(w, "请求页面不存在")
@@ -62,41 +68,14 @@ func articlesStoreHandler(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "body 的值为: %v <br>", body)
 		fmt.Fprintf(w, "body 的长度为: %v <br>", len(body))
 	} else {
-		html := `
-	<!DOCTYPE html>
-	<html lang="en">
-	<head>
-		<title>创建文章 —— 我的技术博客</title>
-		<style type="text/css">.error {color: red;}</style>
-	</head>
-	<body>
-		<form action="{{ .URL }}" method="post">
-			<p><input type="text" name="title" value="{{ .Title }}"></p>
-			{{ with .Errors.title }}
-			<p class="error">{{ . }}</p>
-			{{ end }}
-			<p><textarea name="body" cols="30" rows="10">{{ .Body }}</textarea></p>
-			{{ with .Errors.body }}
-			<p class="error">{{ . }}</p>
-			{{ end }}
-			<p><button type="submit">提交</button></p>
-		</form>
-	</body>
-	</html>
-	`
 		storeURL, _ := route.Get("articles.store").URL()
-
-		data := struct {
-			Title, Body string
-			URL         *url.URL
-			Errors      map[string]string
-		}{
+		data := ArticlesFormData{
 			Title:  title,
 			Body:   body,
 			URL:    storeURL,
 			Errors: errors,
 		}
-		tmpl, err := template.New("create-form").Parse(html)
+		tmpl, err := template.ParseFiles("resources/views/articles/create.gohtml")
 		if err != nil {
 			panic(err)
 		}
@@ -110,23 +89,21 @@ func articlesStoreHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func articlesCreateHandler(w http.ResponseWriter, r *http.Request) {
-	html := `
-	<!DOCTYPE html>
-	<html lang="en">
-	<head>
-		<title>创建文章 —— 我的技术博客</title>
-	</head>
-	<body>
-		<form action="%s" method="post">
-			<p><input type="text" name="title"></p>
-			<p><textarea name="body" cols="30" rows="10"></textarea></p>
-			<p><button type="submit">提交</button></p>
-		</form>
-	</body>
-	</html>
-	`
 	storeURL, _ := route.Get("articles.store").URL()
-	fmt.Fprintf(w, html, storeURL)
+	data := ArticlesFormData{
+		Title:  "",
+		Body:   "",
+		URL:    storeURL,
+		Errors: nil,
+	}
+	tmpl, err := template.ParseFiles("resources/views/articles/create.gohtml")
+	if err != nil {
+		panic(err)
+	}
+	err = tmpl.Execute(w, data)
+	if err != nil {
+		panic(err)
+	}
 }
 
 //定义路由中间件函数
