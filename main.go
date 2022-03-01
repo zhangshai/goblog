dc279054a25458a012332444ae05638bf9466145
zhangshaohai <>
2022-03-01 15:01:47 +0800

    表单验证

--- main.go
+++ main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
+	"net/url"
 	"strings"
+	"unicode/utf8"
 
 	"github.com/gorilla/mux"
 )
@@ -36,14 +39,73 @@ func articlesIndexHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func articlesStoreHandler(w http.ResponseWriter, r *http.Request) {
-	if error := r.ParseForm(); error != nil {
-		fmt.Fprint(w, "请提供正确的数据")
-		return
+
+	title := r.PostFormValue("title")
+	body := r.PostFormValue("body")
+	errors := make(map[string]string)
+	if title == "" {
+		errors["title"] = "标题不能为空"
+	} else if utf8.RuneCountInString(title) < 3 || utf8.RuneCountInString(title) > 40 {
+		errors["title"] = "标题长度需介于 3-40"
 	}
-	title := r.PostForm.Get("title")
-	fmt.Fprintf(w, "POST PostForm: %v <br>", r.PostForm)
-	fmt.Fprintf(w, "POST Form: %v <br>", r.Form)
-	fmt.Fprintf(w, "title 的值为: %v", title)
+
+	if body == "" {
+		errors["body"] = "内容不能为空"
+	} else if utf8.RuneCountInString(body) < 10 {
+		errors["body"] = "内容长度需大于或等于 10 个字节"
+	}
+
+	if len(errors) == 0 {
+		fmt.Fprint(w, "验证通过!<br>")
+		fmt.Fprintf(w, "title 的值为: %v <br>", title)
+		fmt.Fprintf(w, "title 的长度为: %v <br>", len(title))
+		fmt.Fprintf(w, "body 的值为: %v <br>", body)
+		fmt.Fprintf(w, "body 的长度为: %v <br>", len(body))
+	} else {
+		html := `
+	<!DOCTYPE html>
+	<html lang="en">
+	<head>
+		<title>创建文章 —— 我的技术博客</title>
+		<style type="text/css">.error {color: red;}</style>
+	</head>
+	<body>
+		<form action="{{ .URL }}" method="post">
+			<p><input type="text" name="title" value="{{ .Title }}"></p>
+			{{ with .Errors.title }}
+			<p class="error">{{ . }}</p>
+			{{ end }}
+			<p><textarea name="body" cols="30" rows="10">{{ .Body }}</textarea></p>
+			{{ with .Errors.body }}
+			<p class="error">{{ . }}</p>
+			{{ end }}
+			<p><button type="submit">提交</button></p>
+		</form>
+	</body>
+	</html>
+	`
+		storeURL, _ := route.Get("articles.store").URL()
+
+		data := struct {
+			Title, Body string
+			URL         *url.URL
+			Errors      map[string]string
+		}{
+			Title:  title,
+			Body:   body,
+			URL:    storeURL,
+			Errors: errors,
+		}
+		tmpl, err := template.New("create-form").Parse(html)
+		if err != nil {
+			panic(err)
+		}
+
+		err = tmpl.Execute(w, data)
+		if err != nil {
+			panic(err)
+		}
+	}
 
 }
 
@@ -91,8 +153,8 @@ func main() {
 	route.HandleFunc("/about", aboutHandler).Methods("GET").Name("about")
 	route.HandleFunc("/articles/{id:[0-9]+}", articlesShowHandler).Methods("GET").Name("articles.show")
 	route.HandleFunc("/articles", articlesIndexHandler).Methods("GET").Name("articles.index")
-	route.HandleFunc("/articles", articlesStoreHandler).Methods("POST").Name("article.store")
-	route.HandleFunc("/articles/create", articlesCreateHandler).Methods("GET").Name("article.create")
+	route.HandleFunc("/articles", articlesStoreHandler).Methods("POST").Name("articles.store")
+	route.HandleFunc("/articles/create", articlesCreateHandler).Methods("GET").Name("articles.create")
 	route.NotFoundHandler = http.HandlerFunc(notFoundHandler)
 
 	//添加路由中间件
