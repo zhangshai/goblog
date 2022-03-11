1aecd96bbf9463fad352139baf4ba387832cb55f
zhangshaohai <>
2022-03-11 11:19:27 +0800

    注册表单验证

--- app/http/controllers/auth_controller.go
+++ app/http/controllers/auth_controller.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"goblog/app/models/user"
+	"goblog/app/requests"
 	"goblog/pkg/view"
 	"net/http"
 )
@@ -16,23 +17,28 @@ func (*AuthController) Register(w http.ResponseWriter, r *http.Request) {
 func (*AuthController) DoRegister(w http.ResponseWriter, r *http.Request) {
 
 	// 1. 表单验证
-	name := r.PostFormValue("name")
-	email := r.PostFormValue("email")
-	password := r.PostFormValue("password")
-	// 2. 验证通过 —— 入库，并跳转到首页
-
 	_user := user.User{
-		Name:     name,
-		Email:    email,
-		Password: password,
+		Name:            r.PostFormValue("name"),
+		Email:           r.PostFormValue("email"),
+		Password:        r.PostFormValue("password"),
+		PasswordConfirm: r.PostFormValue("password_confirm"),
 	}
-	_user.Create()
-
-	if _user.ID > 0 {
+	errs := requests.ValidateRegistrationForm(_user)
+	if len(errs) > 0 {
+		view.RenderSimple(w, view.D{"Errors": errs, "User": _user}, "auth.register")
 
 	} else {
-		w.WriteHeader(http.StatusInternalServerError)
 
+		//2. 验证通过 —— 入库，并跳转到首页
+
+		_user.Create()
+
+		if _user.ID > 0 {
+
+		} else {
+			w.WriteHeader(http.StatusInternalServerError)
+
+		}
 	}
 
 	// 3. 表单不通过 —— 重新显示表单
